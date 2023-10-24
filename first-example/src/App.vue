2ee1e55e272f78ef98bf4dc9bfcb653ef2448460
chiconasa3000 <csucavelando@gmail.com>
2023-10-24 12:15:20 -0500

    hello vue

---
+++ first-example/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import ElComp from './components/ElComponente.vue'
+</script>
+
+<template>
+  <header>
+    
+  </header>
+
+  <main>
+    <ElComp/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ first-example/src/components/ElComponente.vue
@@ -0,0 +1,74 @@
+<script setup>
+    import {ref} from 'vue'
+    const count = ref(0)
+</script>
+
+<template>
+    <div id="cont_birth">
+        <form id="birth_form">
+            <div id="inputs">
+                <div class="input">
+                    <label for="b_day">Day:</label>
+                    <input type="number" id="b_day" name="b_day" value="5" min="0" max="31"/>
+                </div>
+                <div class="input">
+                    <label for="b_month">Month:</label>
+                    <input type="number" id="b_month" name="b_month" value="12" min="0" max="12"/>
+                </div>
+                <div class="input">
+                    <label for="b_year">Year:</label>
+                    <input type="number" id="b_year" name="b_year" value="1990" min="0" max="3000"/>
+                </div>
+            </div>
+            <div class="button-sub">
+                <button id="birth_button">submit</button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+#birth_form {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+}
+
+#inputs{
+    min-width: 100px;
+    width:100%;
+}
+
+.input{
+    font-size: 1em;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    margin:1em;
+    justify-content: space-between;
+    height: 60px;
+}
+.input input{
+    font-size: 1em;
+}
+
+#birth_form button{
+    font-size: 1.2em;
+    background: white;
+    border:0;
+    border-radius: 1em;
+    padding: 0.5em 1em;
+    margin: .6em .4em;
+}
+
+@media (min-width: 1024px) {
+    #inputs{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+}
+
+</style>
